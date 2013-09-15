de9515a9d8e0e3a38ec0f15112a81c5ec83cad20
cristHian Gz. (gcca) <gcca@gcca.tk>
2013-09-15 00:05:55 -0500

    Mejora en desempeño de captura http.

--- demo/captura/http.py
+++ demo/captura/http.py
@@ -2,8 +2,8 @@ import pylab
 from vi.metodo.captura.http import Captura
 
 def demo():
-    http = Captura()
-    imagen = http.ejecutar('http://190.102.132.123:82/image')
+    http = Captura('http://190.102.132.123:82/image')
+    imagen = http.ejecutar()
 
     pylab.figure()
     pylab.imshow(imagen)
