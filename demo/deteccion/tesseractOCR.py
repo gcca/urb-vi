b4275e5ec5a292101a6147a3695048ebbe1f7e20
cristHian Gz. (gcca) <gcca@gcca.tk>
2013-09-17 22:49:26 -0500

    Removiendo módulos no usados. Aplicando PEP8.

--- demo/deteccion/tesseractOCR.py
+++ demo/deteccion/tesseractOCR.py
@@ -1,10 +1,8 @@
-import cv2.cv as cv
 import cv2
-import pylab
 from vi.metodo.deteccion.tesseractOCR import Deteccion
 
 def demo():
-	i=cv2.imread('demo/deteccion/eurotext.jpg')
-	OCR=Deteccion()
-	texto = OCR.ejecutar(i);
-	print texto
+    i = cv2.imread('demo/deteccion/eurotext.jpg')
+    OCR = Deteccion()
+    texto = OCR.ejecutar(i);
+    print(texto)
--- demo/segmentacion/contorno.py
+++ demo/segmentacion/contorno.py
@@ -1,12 +1,11 @@
-import cv2.cv as cv
 import cv2
 import pylab
 from vi.metodo.segmentacion.contorno import Segmentacion
 
 def demo():
-	i=cv2.imread('demo/segmentacion/carro2.jpg')
-	contorno=Segmentacion()
-	img = contorno.ejecutar(i,100)
-	pylab.figure()
-	pylab.imshow(img)
-	pylab.show()
+    i = cv2.imread('demo/segmentacion/carro2.jpg')
+    contorno = Segmentacion()
+    img = contorno.ejecutar(i, 100)
+    pylab.figure()
+    pylab.imshow(img)
+    pylab.show()
