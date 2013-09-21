b4245c45703e2759f50e0dff286552b8d5b60fbf
cristHian Gz. (gcca) <gcca@gcca.tk>
2013-09-21 17:53:18 -0500

    Recurso: agregar lista de placas generadas

--- recursos/captura.py
+++ recursos/captura.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
 """
@@ -7,9 +8,11 @@ Captura de imágenes de muestra
 import os
 import argparse
 import cv2
+import Tkinter
+import tkSimpleDialog
 
 
-def var_estatica(var_nom, val):
+def var_estt(var_nom, val):
     """Crear variable estática """
     def funda(func):
         """Funda """
@@ -18,21 +21,34 @@ def var_estatica(var_nom, val):
     return funda
 
 
-@var_estatica('val', 100)
+@var_estt('val', 100)
 def intervalo(val):
     """Actualizar intervalo de video """
     intervalo.val = val
 
-@var_estatica('cont', 0)
+@var_estt('cont', 0)
+@var_estt('fich', 123)
 def imagen_guardar(img, dst_dir):
     """Guardar imagen en secuencia  """
-    dst = os.path.join(dst_dir, str(imagen_guardar.cont)) + '.png'
+    cont = str(imagen_guardar.cont)
+    dst = os.path.join(dst_dir, cont) + '.png'
+    placa = tkSimpleDialog.askstring('Ingreso de placa', 'Placa:')
+
     cv2.imwrite(dst, img, [cv2.cv.CV_IMWRITE_PNG_COMPRESSION, 0])
+    imagen_guardar.fich.write('%s,%s\n' % (cont, placa))
     imagen_guardar.cont += 1
 
 def contexto_inic(local_args):
     """Inicializar las variables de contexto """
+    fich = os.path.join(local_args.dir, 'placas.csv')
+
+    Tkinter.Tk().withdraw()
     imagen_guardar.cont = int(local_args.cont_i)
+    imagen_guardar.fich = open(fich, 'w')
+
+def contexto_fin():
+    """Finalizar las variables de contexto """
+    imagen_guardar.fich.close()
 
 def captura(video_ruta, destino_dir):
     """Ciclo de captura """
@@ -70,7 +86,7 @@ def inic():
     """Inicio de ejecución """
     parser = argparse.ArgumentParser(
         description=u'Captura de imágenes de muestra',
-        epilog='[A] Capturar <-> [:SPACE:] Pausa <-> [:ESC:] Salir')
+        epilog=u'[A] Capturar - [:SPACE:] Pausa - [:ESC:] Salir')
 
     parser.add_argument('--video',
                         help='Ruta al fichero de video',
@@ -84,12 +100,12 @@ def inic():
 
     args = parser.parse_args()
 
-    assert os.path.exists(args.dir), 'No existe el directorio'
+    assert os.path.exists(args.dir), 'No existe el directorio ' + args.dir
     assert args.cont_i.isdigit(), '`cont_i` debe ser un número'
 
     contexto_inic(args)
-
     captura(args.video, args.dir)
+    contexto_fin()
 
 if __name__ == '__main__':
     inic()
