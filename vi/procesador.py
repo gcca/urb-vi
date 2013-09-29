828d7cc919d35382ff261b913da355045782f5f0
cristHian Gz. (gcca) <gcca@gcca.tk>
2013-09-29 13:55:58 -0500

    procesador: docstring

--- vi/procesador.py
+++ vi/procesador.py
@@ -2,6 +2,9 @@
 # encoding: utf-8
 """
 vi.procesador -- brief
+
+La variable `procesos` mantiene el orden de lo métodos empleados: captura,
+
 """
 
 import importlib
@@ -9,7 +12,10 @@ import importlib
 procesos = []
 
 def ejecutar(resultado=None):
-    """() -> NoneType """
+    """(object) -> NoneType
+
+    Primer parámetro de proceso
+    """
 
     for proceso in procesos:
         resultado = proceso.ejecutar(resultado)
@@ -17,6 +23,22 @@ def ejecutar(resultado=None):
     return resultado
 
 def inic(args):
+    """() -> NoneType
+
+    Recaba las clase definidas en `args`
+
+    'fichero'    Imagen         Imagen           Arreglo             Placas
+       +---------+  +-----------+  +--------------+  +----------------+
+     o-| Captura |->| Deteccion |->| Segmentacion |->| Reconocimiento |-@
+       +---------+  +-----------+  +--------------+  +----------------+
+
+    Args:
+        args - Objeto con atributos de los métodos
+        >>> args.met_captura = 'demo'
+        >>> args.met_deteccion = 'demo'
+        >>> args.met_segmentacion = 'demo'
+        >>> args.met_reconocimiento = 'demo'
+    """
     for nombre in ['captura', 'deteccion', 'segmentacion', 'extraccion']:
         metodo = getattr(args, 'met_' + nombre)
         modulo = importlib.import_module('vi.metodo.' + nombre + '.' + metodo)
