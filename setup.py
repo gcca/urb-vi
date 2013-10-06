e95f49e3854d922d44466840c3f7460b8cdac061
cristHian Gz. (gcca) <gcca@gcca.tk>
2013-10-06 10:10:22 -0500

    Dependencias

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+"""
+SetupTools
+"""
+from setuptools import setup, find_packages
+setup(name="urb-vi",
+      version="0.1",
+      install_requires = ['unittest2', 'mock'],
+      packages=find_packages())
