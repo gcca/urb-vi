fb4a8c6958b60e5e69e7e16f999db85555ce5cec
cristHian Gz. (gcca) <gcca@gcca.tk>
2013-10-07 11:45:12 -0500

    setup.py para dependencias

--- setup.py
+++ setup.py
@@ -5,5 +5,4 @@ SetupTools
 from setuptools import setup, find_packages
 setup(name="urb-vi",
       version="0.1",
-      install_requires = ['unittest2', 'mock'],
-      packages=find_packages())
+      install_requires = ['unittest2', 'mock'])
