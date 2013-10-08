0204e20e27535c9143b5a8a65d81ef5dce0b55b5
cristHian Gz. (gcca) <gcca@gcca.tk>
2013-10-08 08:27:26 -0500

    Remueve función no usado

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 """
 SetupTools
 """
-from setuptools import setup, find_packages
+from setuptools import setup
+
 setup(name="urb-vi",
       version="0.1",
       install_requires = ['unittest2', 'mock'])
