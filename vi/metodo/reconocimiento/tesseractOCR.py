21e6bd88471714b916d712d0f3a6febc7720f6a0
cristHian Gz. (gcca) <gcca@gcca.tk>
2013-10-10 18:37:34 -0500

    Linter

--- vi/metodo/reconocimiento/tesseractOCR.py
+++ vi/metodo/reconocimiento/tesseractOCR.py
@@ -1,62 +1,44 @@
-# -*- coding: utf-8 -*-
+# encoding: utf-8
 
-"""
-Método reconocimiento tesseractOCR
-"""
+"""Método reconocimiento tesseractOCR. """
 
-import cv2.cv as cv
-import cv2
+from cv2 import cv
 import tesseract
-
+import re
 
 class Reconocimiento(object):
-	"""
-	reconocimiento
-	"""
-	def __init__(self):
-		self.dicc = {};
-		self.arrayPlacas=[];
+    """Reconocimiento. """
 
-	def leerPlaca(self,img):
-		"""
-        Args:
-            img - imagen que contiene el texto, obtenida con la funcion cv2.imread
-
-        Rets:
-            El string del texto en la imagen
+    def leerPlaca(self, binarizado):
         """
-		try:
-			gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		except IOError:
-			#Imagen obtenida con la funcion del cv
-			raise AttributeError
-		else:
-			#binarizacion de la imagen y cambio de estructura
-			_, binarizado = cv2.threshold(gris, 100, 255, cv2.THRESH_BINARY)
-			cvmat_image=cv.fromarray(binarizado)
-			imgbincv =cv.GetImage(cvmat_image)
+        Args:
+            img: imagen que contiene el texto, obtenida con la
+                funcion cv2.imread.
+        Returns:
+            El string del texto en la imagen.
+        """
+        cvmat_image = cv.fromarray(binarizado)
+        imgbincv = cv.GetImage(cvmat_image)
+        # GetImage fuente de un error, revisar
 
-			#configuracion del tesseract
-			api = tesseract.TessBaseAPI()
-			api.Init(".","eng",tesseract.OEM_DEFAULT)
-			api.SetPageSegMode(tesseract.PSM_AUTO)
+        # Configuración del tesseract
+        api = tesseract.TessBaseAPI()
+        api.Init(".", "eng", tesseract.OEM_DEFAULT)
+        api.SetPageSegMode(tesseract.PSM_AUTO)
 
-			#enviando imagen binarizada al tesseract
-			tesseract.SetCvImage(imgbincv,api)
-			text=api.GetUTF8Text()
-			return text
+        # Enviando imagen binarizada al tesseract
+        text = ''
+        tesseract.SetCvImage(imgbincv, api)
+        text = api.GetUTF8Text()
+        text = re.sub(r'\W+', '', text)
+        return text
 
-	def generarDiccionario(self,img):
-		self.dicc[img]=self.leerPlaca(img);
-
-	def ejecutar(self,arrayImg):
-		"""
+    def ejecutar(self, baldosas):
+        """
         Args:
             arrayImg - Arrego de imagenes, obtenidas con la funcion cv2.imread
 
         Rets:
             array de las placas
         """
-		for img in arrayImg:
-			self.arrayPlacas.append(self.leerPlaca(img));
-		return self.arrayPlacas;
+        return [self.leerPlaca(baldosa) for baldosa in baldosas]
