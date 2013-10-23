a524482ee87ef5bd1821ac8a0a53f8bff3ba1dc8
cristHian Gz. <gcca@gcca.tk>
2013-10-22 20:37:21 -0500

    Detección: mejora en esquema de filtros

--- vi/metodo/deteccion/contorno.py
+++ vi/metodo/deteccion/contorno.py
@@ -2,6 +2,8 @@
 
 """Detección por búsqueda de contornos """
 from __future__ import division
+
+import numpy as np
 import cv2
 
 
@@ -10,23 +12,25 @@ class Deteccion(object):
 
     def __init__(self):
         """Inicia la lista de filtros """
-        self._filtros = [self.filtro_npixeles,
-                         self.filtro_recthorizontal]
+        self.imagen = None
+        def isfilter(attrname, methodclass):
+            """¿El método es filtro? """
+            return (attrname.startswith('filtro')
+                    and hasattr(getattr(methodclass, attrname), '__class__'))
+        self._filtros = [getattr(self, method) \
+                         for method in dir(self) if isfilter(method, self)]
 
     def ejecutar(self, imagen):
         """Recibe una imagen y retorna una lista con las secciones limitadas
         por los contornos """
+        self.imagen = imagen
         imgris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
         _, umbral = cv2.threshold(imgris, 100, 255, cv2.THRESH_BINARY)
         contornos, _ = cv2.findContours(umbral,
                                         cv2.RETR_TREE,
                                         cv2.CHAIN_APPROX_NONE)
-
         filtrados = []
         for contorno in contornos:
-            # (-o-)
-            # Implementación temporal
-            # -> Validar en cascada
             if all(self.validar(contorno)):
                 filtrados.append(contorno)
 
@@ -41,17 +45,46 @@ class Deteccion(object):
         >>> list(self.validar(contorno))
         [True, False]
         """
-        return (bool(filtrar(contorno)) for filtrar in self._filtros)
+        return (filtrar(contorno) for filtrar in self._filtros)
 
     @staticmethod
-    def filtro_npixeles(contorno):
+    def filtro_a_num_pixel(contorno):
         """Filtro simple
         No considerar como válido porque solo aplica a la imagen de la demo
         """
         return 200 < len(contorno)
 
     @staticmethod
-    def filtro_recthorizontal(contorno):
+    def filtro_b_rect_horiz(contorno):
         """Verifica que el contorno contiene un rectángulo horizontal """
         _, _, ancho, alto = cv2.boundingRect(contorno)
         return alto < ancho/2.5
+
+    def filtro_c_amarillo_blanco(self, contorno):
+        """Filtro de color amarillo y blanco """
+        imagen = self.imagen
+        x, y, dx, dy = cv2.boundingRect(contorno)
+        baldosa = imagen[y:(y+dy), x:(x+dx)]
+
+        # _ = cv2.medianBlur(baldosa, 5)
+        # hsv = cv2.cvtColor(baldosa, cv2.COLOR_BGR2HSV)
+        hsv = baldosa
+
+        a = cv2.inRange(hsv, np.array((20, 100, 100)), np.array((30, 255, 255)))
+        b = cv2.inRange(hsv, np.array((100,100,10)), np.array((130, 130, 130)))
+        mascara = cv2.add(a, b)
+
+        erode = cv2.erode(mascara, None, iterations=1)
+        dilate = cv2.dilate(erode, None, iterations=3)
+
+        dilate = cv2.cvtColor(dilate, cv2.COLOR_GRAY2BGR)
+
+        num_neg = (dilate == 0).sum()
+        num_pos = dilate.size - num_neg
+        ratio = num_pos/dilate.size
+
+        # if ratio > 0.7:
+        #     cv2.imshow('vii', np.hstack((dilate, hsv)))
+        #     cv2.waitKey()
+
+        return ratio > 0.7
