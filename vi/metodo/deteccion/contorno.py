1b1e633c300a70c245c6f6b422a1a9fece92924a
cristHian Gz. (gcca) <gcca@gcca.tk>
2013-11-15 21:56:55 -0500

    Util: umbrales y contornos.

--- vi/metodo/deteccion/contorno.py
+++ vi/metodo/deteccion/contorno.py
@@ -3,9 +3,9 @@
 """Detección por búsqueda de contornos."""
 from __future__ import division, print_function
 
-import numpy as np
 import cv2
-from vi.util import dibujar_rectangulos, dibujar_contornos
+from vi.util import dibujar_rectangulos, dibujar_contornos, \
+    generar_umbrales, hallar_contornos
 
 
 class Deteccion(object):
@@ -14,11 +14,13 @@ class Deteccion(object):
     def __init__(self):
         """Inicia la lista de filtros."""
         self.imagen = None
+
         def isfilter(attrname, methodclass):
             """¿El método es filtro?."""
             return (attrname.startswith('filtro')
                     and hasattr(getattr(methodclass, attrname), '__class__'))
-        self._filtros = [getattr(self, method) \
+
+        self._filtros = [getattr(self, method)
                          for method in dir(self) if isfilter(method, self)]
 
     def ejecutar(self, imagen):
@@ -37,51 +39,10 @@ class Deteccion(object):
 
     def __contornos(self):
         """Obtener contornos de la imagen."""
-        def umbral_adaptativo(imagen,  # Umbral simple
-                              valor_max,  # umbral
-                              metodo_adaptativo,  # valor_max
-                              tipo_umbral,
-                              dim_bloque=None,  # centinela
-                              C=None):
-            """Calcula umbral simple o adaptativo según parámetros."""
-            if dim_bloque:
-                return cv2.adaptiveThreshold(imagen,
-                                             valor_max,
-                                             metodo_adaptativo,
-                                             tipo_umbral,
-                                             dim_bloque,
-                                             C)
-            else:
-                _, imagen_bin = cv2.threshold(imagen,
-                                              valor_max,
-                                              metodo_adaptativo,
-                                              tipo_umbral)
-                return imagen_bin
-
-        gris = cv2.cvtColor(self.imagen, cv2.COLOR_BGR2GRAY)
-        configs = [
-            # (gris, 100, 255, cv2.THRESH_BINARY),  # (-o-) Debería retirarse
-            (gris, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
-             cv2.THRESH_BINARY, 13, 6),
-            (gris, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-             cv2.THRESH_BINARY, 31, 4),  # 13 7
-        ]
-        umbrales = [umbral_adaptativo(*c) for c in configs]
-
-        if __debug__:
-            print('1. Umbrales')
-            # int(escala * dimension)
-            dim = tuple(int(0.4*d) for d in reversed(gris.shape))
-            cv2.imshow('det', np.vstack(cv2.resize(u, dim) for u in umbrales))
-            cv2.waitKey()
-
-        # Mezcla de contornos
-        def hallar_contornos(umbral):
-            """Envoltura de findContours."""
-            return cv2.findContours(umbral, cv2.RETR_TREE,
-                                    cv2.CHAIN_APPROX_NONE)[0]
-        mezcla = [hallar_contornos(umbral) for umbral in umbrales]
-        contornos = [contorno for contornos in mezcla for contorno in contornos]
+        umbrales = generar_umbrales(self.imagen, 13, 6, 31, 4)
+        contornos = hallar_contornos(umbrales,
+                                     cv2.RETR_TREE,
+                                     cv2.CHAIN_APPROX_NONE)
 
         if __debug__:
             print('2. Contornos')
--- vi/util.py
+++ vi/util.py
@@ -2,6 +2,7 @@
 """Util """
 
 from random import randint as randz
+import numpy as np
 import cv2
 
 
@@ -34,3 +35,76 @@ def dibujar_rectangulos(imagen, contornos):
     """
     for x, y, ancho, alto in contornos:
         cv2.rectangle(imagen, (x, y), (x+ancho, y+alto), __color(), 2)
+
+
+def generar_umbrales(imagen, long_bloque, c, long_bloque_p=None, c_p=None):
+    """Generar un objeto iterable con distintas imágenes 'umbralizadas'.
+
+    Args:
+        imagen
+            Imagen base para las imagens 'umbralizadas'.
+        long_bloque
+            Longitud del bloque para calcular el umbral.
+        c
+            Constante que se resta al umbral.
+        long_bloque_p
+            `long_bloque` para generar la siguiente imagen umbralizada.
+        c_p
+            `c` para generar la siguiente imagen umbralizada.
+
+    Returns:
+        Iterable con los contornos.
+    """
+    if not long_bloque_p:
+        long_bloque_p = long_bloque
+        c = c_p
+
+    gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    umbral_1 = cv2.adaptiveThreshold(gris,
+                                     255,
+                                     cv2.ADAPTIVE_THRESH_MEAN_C,
+                                     cv2.THRESH_BINARY,
+                                     long_bloque,
+                                     c)
+    umbral_2 = cv2.adaptiveThreshold(gris,
+                                     255,
+                                     cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                     cv2.THRESH_BINARY,
+                                     long_bloque_p,
+                                     c_p)
+    if __debug__:
+        print('- Umbrales')
+        # int(escala * dimension)
+        dim = tuple(int(0.4*d) for d in reversed(gris.shape))
+        cv2.imshow('umbrales',
+                   np.vstack((cv2.resize(umbral_1, dim),
+                              cv2.resize(umbral_2, dim))))
+        cv2.waitKey()
+        cv2.destroyWindow('umbrales')
+
+    return umbral_1, umbral_2
+
+
+def hallar_contornos(umbrales, modo, metodo):
+    """Hallar contornos.
+
+    Args:
+        umbrales
+            Iterable de imágenes umbralizadas.
+        modo
+            Modo de recuperación de contornos. Mirar la documentación.
+                CV_RETR_EXTERNAL
+                CV_RETR_LIST
+                CV_RETR_CCOMP
+                CV_RETR_TREE
+        metodo
+            Método de aproximación de contornos. Mirar la documentación.
+                CV_CHAIN_APPROX_NONE
+                CV_CHAIN_APPROX_SIMPLE
+                CV_CHAIN_APPROX_TC89_L1, CV_CHAIN_APPROX_TC89_KCOS
+    Returns:
+        Lista de contornos.
+    """
+    mezcla = [cv2.findContours(umbral, modo, metodo)[0]
+              for umbral in umbrales]
+    return [contorno for contornos in mezcla for contorno in contornos]
